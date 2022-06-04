fcd68020e61cbb8adbba61183148aa1ed1fb8694
Max <zerex290@gmail.com>
2022-06-04 21:02:18 +0300

    Minor code formatting in blueprints/admin
    Create class inherited from Exception
    Make exception catching more explicit

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -9,6 +9,10 @@ from bot.rules import AdminRule
 bp = Blueprint('AdminCommands')
 
 
+class UnregisteredExceptions(Exception):
+    pass
+
+
 @bp.on.message(AdminRule(admins=(191901652, 687594282)))
 async def execute(message: Message, postgres: bool) -> None:
     code: str = message.text.lstrip('!execpg').lstrip('!exec').lstrip()
@@ -16,17 +20,17 @@ async def execute(message: Message, postgres: bool) -> None:
         try:
             exec(f"async def _aexecute(message):" + ''.join([f"\n    {line}" for line in code.split('\n')]))
             await locals()['_aexecute'](message)
-        except:
+        except UnregisteredExceptions:
             exception = traceback.format_exc()
             exception = exception[-4096:] if len(exception) > 4096 else exception
             await message.answer(f"Ошибка: {exception}")
-        return
+        return None
 
     async with PostgresConnection() as connection:
         try:
             exec(f"async def _aexecute(message, connection):" + ''.join([f"\n    {line}" for line in code.split('\n')]))
             await locals()['_aexecute'](message, connection)
-        except:
+        except UnregisteredExceptions:
             exception = traceback.format_exc()
             exception = exception[-4096:] if len(exception) > 4096 else exception
             await message.answer(f"Ошибка: {exception}")
