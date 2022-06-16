60a7d052b1998eb3f6e009943ab6cad560b11aae
Max <zerex290@gmail.com>
2022-06-16 18:37:05 +0300

    Update execute function code

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -9,28 +9,28 @@ from bot.rules import AdminRule
 bp = Blueprint('AdminCommands')
 
 
-class UnregisteredExceptions(Exception):
+class UnregisteredException(Exception):
     pass
 
 
 @bp.on.message(AdminRule(admins=(191901652, 687594282)))
 async def execute(message: Message, postgres: bool) -> None:
     code: str = message.text.lstrip('!execpg').lstrip('!exec').lstrip()
-    if not postgres:
-        try:
-            exec(f"async def _aexecute(message):" + ''.join([f"\n    {line}" for line in code.split('\n')]))
+    try:
+        if postgres:
+            async with PostgresConnection() as connection:
+                exec(
+                    f"async def _aexecute(message, connection):"
+                    + ''.join([f"\n    {line}" for line in code.split('\n')])
+                )
+                await locals()['_aexecute'](message, connection)
+        else:
+            exec(
+                f"async def _aexecute(message):"
+                + ''.join([f"\n    {line}" for line in code.split('\n')])
+            )
             await locals()['_aexecute'](message)
-        except UnregisteredExceptions:
-            exception = traceback.format_exc()
-            exception = exception[-4096:] if len(exception) > 4096 else exception
-            await message.answer(f"Ошибка: {exception}")
-        return None
-
-    async with PostgresConnection() as connection:
-        try:
-            exec(f"async def _aexecute(message, connection):" + ''.join([f"\n    {line}" for line in code.split('\n')]))
-            await locals()['_aexecute'](message, connection)
-        except UnregisteredExceptions:
-            exception = traceback.format_exc()
-            exception = exception[-4096:] if len(exception) > 4096 else exception
-            await message.answer(f"Ошибка: {exception}")
+    except UnregisteredException:
+        exc = traceback.format_exc()
+        exc = exc[-4096:] if len(exc) > 4096 else exc
+        await message.answer(f"Ошибка: {exc}")
