d7e89f20c2579740c299341cbfcfb16b52ef6c49
Max <zerex290@gmail.com>
2022-08-20 09:34:06 +0300

    Format book information template

--- bot/templates/honeyimpact/books.py
+++ bot/templates/honeyimpact/books.py
@@ -5,7 +5,7 @@ def format_information(book: books.Information) -> str:
     formatted_information = (
         f"🖼Основная информация:\n"
         f"📚Серия книг: {book.name}\n"
-        f"📗Номер тома: {book.volume}\n"
+        f"📗Часть: {book.volume}\n"
         f"📖Содержание:\n{book.story}"
     )
     return formatted_information
